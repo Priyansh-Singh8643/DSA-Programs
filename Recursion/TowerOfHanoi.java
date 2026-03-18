66d2613bbe2734d2724acb9b119b0759256caf65
Priyansh-Singh8643 <priyanshsingh7500@gmail.com>
2026-03-18 22:47:52 +0530

    Seventh Commit

--- Recursion/TowerOfHanoi.java
+++ Recursion/TowerOfHanoi.java
@@ -10,12 +10,20 @@ class  TowerOfHanoi
 			return;
 		}
 		
-		solve(n-1,source,dest,aux)
-			
-		solve(
+		solve(n-1,source,dest,aux);
+		
+		System.out.printf("Move disk %d from rod %c to rod %c%n",n,source,dest);
+		
+		solve(n-1, aux, source, dest);
 	}
 	public static void main(String[] args) 
 	{
+		Scanner sc=new Scanner(System.in);
 		
+		System.out.println("Enter the number of disks : ");
+		int n=sc.nextInt();
+		solve(n,'S','A','D');
+		
+		sc.close();
 	}
 }
